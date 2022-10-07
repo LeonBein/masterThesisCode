b3f43b2288170ae956ea723fe1bd96b080699f0f
Leon Bein <leon.bein@student.hpi.de>
2022-10-07 10:44:24 +0200

    add infrastructure

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# https://jupyterlab.readthedocs.io/en/stable/getting_started/installation.html
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Make sure pip is up to date to install jupyterlab
+pip3 install -U pip
+
+pip install jupyterlab
+jupyter lab
+
+# Other installations
+pip install PyGithub
+pip install python-dotenv # Optional, for keeping access tokens out of the notebook
+pip install pandas
+pip install matplotlib
---
+++ start_jupyter.bat
@@ -0,0 +1,2 @@
+bash -c "source .venv/bin/activate; jupyter lab"
+pause
